85470bfde54da2e10a7e2e0e1743cacc817a3483
vrusso300 <vrusso300@caledonian.ac.uk>
2025-08-04 15:34:06 +0100

    First commit from iDE

---
+++ fl0w-backend/src/main/java/dev/vinty/fl0w/Fl0wBackendApplication.java
@@ -0,0 +1,15 @@
+package dev.vinty.fl0w;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fl0wBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fl0wBackendApplication.class, args);
+
+
+	}
+
+}
---
+++ fl0w-backend/src/test/java/dev/vinty/fl0w/Fl0wBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vinty.fl0w;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fl0wBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
